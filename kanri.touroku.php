78401fae52b0e4826b6f762caf994ae6e294f09a
nagisa0308 <2301390@s.asojuku.ac.jp>
2024-11-18 10:21:19 +0900

    1回目

---
+++ kanri.touroku.php
@@ -0,0 +1,28 @@
+//管理者新規登録画面(admin_register.html)のソースコード
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css"/>
+    <title>管理者新規登録</title>
+    
+</head>
+<body>
+    <h1>管理者新規登録</h1>
+    <form action="login.html" method="get">
+        <label>パスワード</label>
+        <input type="password" name="password" required>
+        
+        <label>ユーザー名</label>
+        <input type="text" name="username" required>
+        
+        <label>管理者ID</label>
+        <input type="text" name="admin_id" required>
+        
+        <!-- 登録ボタンとキャンセルボタン -->
+        <button type="submit" class="button-common button-blue">登録</button>
+        <button type="button" class="button-common button-gray" onclick="window.location.href='roguin.php'">キャンセル</button>
+    </form>
+</body>
+</html>
---
+++ roguin.php
@@ -0,0 +1,25 @@
+//ログイン画面のソースコード
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css"/>
+    <title>中央にフォームとボタンを表示</title>
+</head>
+<body>
+    <div class="container">
+        <div>メールアドレス・または管理者ID</div>
+        <input type="text" placeholder="入力してください">
+        
+        <div>パスワード</div>
+        <input type="password" placeholder="パスワードを入力してください">
+        
+        <button class="button login-button">ログイン</button>
+        <button class="button register-button">新規登録</button>
+        <button class="button admin-register-button">管理者新規登録</button>
+        
+        <div class="note">※一般ユーザーは使用できません</div>
+    </div>
+</body>
+</html>
